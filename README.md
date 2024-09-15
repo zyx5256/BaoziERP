369f47c55eeb18aef40a58d6df357edef5b1f989
Zhu Yuxiao <zyx4us@gmail.com>
2024-09-15 09:40:35 +0800

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+A ERP System
--- main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5 import QtWidgets
-from ui.main_page import MainPage
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    ex = MainPage()
-    sys.exit(app.exec_())
